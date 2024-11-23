c00e7230bf109161979f91859ccbcbcfd8490381
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-11-23 03:38:57 +0200

    Implemented _hash_password() func

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 "Auth File"
+from db import DB
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, str(hashed_password))
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
 
 
 def _hash_password(password: str) -> bytes:

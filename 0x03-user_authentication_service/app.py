1c4d6d01409de70b955f9f64a53877e5f7b7db6d
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-11-23 04:26:26 +0200

    Implemented a registration function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, request
+from auth import Auth
 
 
+auth = Auth()
 app = Flask(__name__)
 
 
@@ -9,6 +11,22 @@ app = Flask(__name__)
 def hello_world():
     """Dummy function"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register():
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+    except KeyError:
+        abort(400)
+
+    try:
+        user = auth.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": user.email, "message": "user created"})
 
 
 if __name__ == "__main__":

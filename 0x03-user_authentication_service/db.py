d9bf0d420542c0c8daf5061c875f45d0aefb47f1
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-11-23 02:52:14 +0200

    Created the DataBase

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+DataBase Class File
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User, Base
+
+
+class DB:
+    """
+    DB Definitions
+    """
+
+    def __init__(self) -> None:
+        """
+        DB constructor
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a user object to the db
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
